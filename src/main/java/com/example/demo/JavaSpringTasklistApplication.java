e0d436865ab3b2bb1c52eea80963e2d911c8d38c
Jozef W. <jozko.waldo@gmail.com>
2025-04-18 13:20:55 +0200

    Initial Commit - Base Code

---
+++ src/main/java/com/example/demo/JavaSpringTasklistApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTasklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTasklistApplication.class, args);
+	}
+
+}
